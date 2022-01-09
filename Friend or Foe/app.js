bbee1c37ccde626c742e24cb24f05d2ce83a6307
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-01-09 16:06:57 +0100

    done

---
+++ Friend or Foe/app.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+function friend(friends) {
+	//your code here
+	let newArr = [];
+	friends.filter((friend) => {
+		if (friend.length == 4) {
+			newArr.push(friend);
+		}
+	});
+	return newArr;
+}
+friend(['Ryan', 'Kieran', 'Mark']);
