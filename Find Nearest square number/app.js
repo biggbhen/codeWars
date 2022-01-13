027c89051390d40c6e3f2072ab700591b17de336
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-01-13 15:12:33 +0100

    done

---
+++ Find Nearest square number/app.js
@@ -0,0 +1,6 @@
+// find the nearest square number
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+const nearestSq = (n) => Math.pow(Math.round(Math.sqrt(n)), 2);
+
+nearestSq(10);
