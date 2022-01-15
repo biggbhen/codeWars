3cbbdf4bfe87bd45a30a4e7b6f61438ed2b27e96
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-01-15 13:29:53 +0100

    done

---
+++ double char/app.js
@@ -0,0 +1,15 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+	return str
+		.split('')
+		.map(function (v) {
+			return v + v;
+		})
+		.join('');
+}
