3204fdfe4ad6f9867695d2c6b8ae90b8335ed45d
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-04 20:16:14 +0100

    almost sorted out

--- find divisors/main.js
+++ find divisors/main.js
@@ -1,21 +1,20 @@
 // Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
 
 function divisors(integer) {
-  let numbArray=[];
-  
-for(let i= 2; i < integer; i++ ){
- if(integer % i == 0){
-numbArray.push(i)
-console.log(numbArray);
+  let numbArray = [];
+  let isPrime;
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) {
+      numbArray.push(i);
+      // console.log(numbArray);
+    }
+  }
+   if (numbArray != []){
+      console.log(`${integer} is not prime`);
+    }
+    else {
+      console.log(`${integer} is prime`);
+    }
+  console.log(numbArray);
 }
-
- if(integer % i == 0){
- console.log(`${integer} is not prime`);
-}else{
- console.log(`${integer} is not prime`);
-}
-}
-
-// console.log(numbArray);
-};
-divisors(13)
+divisors(13);
