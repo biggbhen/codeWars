015c6a1046ae21a2159112960e05d97809417e40
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-13 15:53:28 +0100

    done

--- convert string to camelcase/main.js
+++ convert string to camelcase/main.js
@@ -4,36 +4,19 @@
 // "the-stealth-warrior" gets converted to "theStealthWarrior"
 // "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 
-// function toCamelCase(str){
-// let newArr = []
-// for (let x = 0; x < str.length; x++) {
-//  let element = str[x];
-// if (element == '_') {
-//  element = ' '
-// }
-// newArr.push(element)
-// }
-// newArr.join('');
-
-// let camelStr = newArr.s
-// }
-// toCamelCase("the_stealth_warrior")
 
 
 
 function toCamelCase(str){
-let newArr = str.split('')
-joinStr=[]
-for(let i = 0; i < newArr.length; i++){
- let x = newArr[i]
- if(x === '-' || x === '_'){
-   x = ''
-  }
-  joinStr.push(x)
- }
- console.log(joinStr.join(''));
-// console.log(newArr);
-// console.log(typeof(str));
-
+let newArr = str.replace(/[^a-z0-9]/gi,' ').split(' ') 
+capitalizer = []
+let joinArr =[]
+for(let i = 1; i < newArr.length; i++){
+ let x = newArr[i];
+ let toCap = x[0].toUpperCase() + x.slice(1, x.length)
+capitalizer.push(toCap)
+} 
+joinArr.push(newArr[0], ...capitalizer);
+return joinArr.join('')
 }
-toCamelCase("the_stealth_warrior"); 
+console.log(toCamelCase("the_stealth_warrior")); 
