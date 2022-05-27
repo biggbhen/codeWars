eac2c9eaaf5b929f4535daefa79ee5f4aa377252
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-27 16:59:22 +0100

    done

---
+++ is the string uppercase/app.js
@@ -0,0 +1,22 @@
+// Create a method to see whether the string is ALL CAPS.
+/*Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True */
+
+isUpperCase = function (str) {
+  let element;
+  let arrStr = str.split('');
+  const newstr = arrStr.filter((i) => i != ' ');
+  for (let i = 0; i < newstr.length; i++) {
+    element = newstr[i];
+    if (element !== element.toUpperCase()) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isUpperCase('HELLO I AM DONALD'));
