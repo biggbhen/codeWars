dcee364f64123ecea1dcc671920b4f8d6f42ffc7
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-27 22:06:07 +0100

    done

--- is the string uppercase/app.js
+++ is the string uppercase/app.js
@@ -7,16 +7,20 @@
 "ACSKLDFJSgSKLDFJSKLDFJ" -> False
 "ACSKLDFJSGSKLDFJSKLDFJ" -> True */
 
-isUpperCase = function (str) {
-  let element;
-  let arrStr = str.split('');
-  const newstr = arrStr.filter((i) => i != ' ');
-  for (let i = 0; i < newstr.length; i++) {
-    element = newstr[i];
-    if (element !== element.toUpperCase()) {
-      return false;
-    }
-  }
-  return true;
+// isUpperCase = function (str) {
+//   let element;
+//   let arrStr = str.split('');
+//   const newstr = arrStr.filter((i) => i != ' ');
+//   for (let i = 0; i < newstr.length; i++) {
+//     element = newstr[i];
+//     if (element !== element.toUpperCase()) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// console.log(isUpperCase('HELLO I AM DONALD'));
+
+String.prototype.isUpperCase = function () {
+  return this == this.toUpperCase();
 };
-console.log(isUpperCase('HELLO I AM DONALD'));
