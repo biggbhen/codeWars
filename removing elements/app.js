d34ee97136aed4277a207720ca128b8f8581995b
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-27 22:38:45 +0100

    done

---
+++ removing elements/app.js
@@ -0,0 +1,20 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr) {
+  let newArr = [];
+  // console.log(arr);
+  for (let i = 0; i < arr.length; i += 2) {
+    const element = arr[i];
+    newArr.push(element);
+  }
+  return newArr;
+}
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
+
+// ['Hello', 'Hello Again'];
