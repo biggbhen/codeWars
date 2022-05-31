ab574bd7a4bc0669206ce3c689d5f51a93eaa660
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-31 17:36:39 +0100

    done

--- Replace all items/app.js
+++ Replace all items/app.js
@@ -6,17 +6,38 @@
 
 // replaceAll(replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
 
-function replaceAll(seq, find, replace) {
-  seq.forEach((item, index) => {
-    if (item === find) {
-      seq[index] = replace;
-    }
-  });
-  return seq;
+// function replaceAll(seq, find, replace) {
+//   seq.forEach((item, index) => {
+//     if (item === find) {
+//       seq[index] = replace;
+//     }
+//   });
+//   return seq;
+// }
+
+function replaceAll(seq, find, rep) {
+  if (typeof seq === 'string') {
+    return seq
+      .split('')
+      .filter((item) => {
+        item === find ? (item = rep) : item;
+      })
+      .join('');
+  }
+  return seq.map((v) => (v === find ? (v = replace) : v));
 }
 
 // replaceAll([], 1, 2);
 // [];
-console.log(replaceAll([1, 2, 2], 1, 2));
+console.log(replaceAll('122', 1, 2));
+// replaceAll('122', 1, 2);
 
 // [2, 2, 2];
+// function replaceAll(seq, find, replace) {
+//   if (typeof seq === 'string')
+//     return seq
+//       .split('')
+//       .map((v) => (v === find ? (v = replace) : v))
+//       .join('');
+//   return seq.map((v) => (v === find ? (v = replace) : v));
+// }
