e414d364ffe9cb8e64b381e235052907c74338e4
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-31 14:20:39 +0100

    done

--- Replace all items/app.js
+++ Replace all items/app.js
@@ -5,3 +5,18 @@
 // Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
 
 // replaceAll(replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
+
+function replaceAll(seq, find, replace) {
+  seq.forEach((item, index) => {
+    if (item === find) {
+      seq[index] = replace;
+    }
+  });
+  return seq;
+}
+
+// replaceAll([], 1, 2);
+// [];
+console.log(replaceAll([1, 2, 2], 1, 2));
+
+// [2, 2, 2];
