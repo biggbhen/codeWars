0afef339c9214d1a7952984e1002a7e910fbe599
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-23 18:17:15 +0100

    the answer

--- find the unique number/app.js
+++ find the unique number/app.js
@@ -13,11 +13,9 @@
 // Find The Unique
 
 function findUniq(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let ans = arr.filter((x) => arr[i] == x).length;
-    if (ans == 1) {
-      return arr[i];
-    }
+  function findUniq(arr) {
+    arr.sort((a, b) => a - b);
+    return arr[0] == arr[1] ? arr.pop() : arr[0];
   }
 }
 console.log(findUniq([1, 1, 2, 1, 1]));
