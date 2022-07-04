0dcf702b104490545d1f6e5090aa14a94e0dfc92
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-04 05:42:42 +0100

    done

--- Welcome!/app.js
+++ Welcome!/app.js
@@ -89,3 +89,27 @@ function greet(language) {
 }
 
 greet('english');
+
+/**function greet(lang) {
+  return langs[lang]||langs['english'];
+}
+
+var langs = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+}; */
