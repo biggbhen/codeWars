c03078f7af0d24300614d671344ebed5d012f769
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-12-29 23:44:38 +0100

    done

---
+++ escape the rooms/app.js
@@ -0,0 +1,8 @@
+// Escape the room
+// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+
+var rooms = {
+	room1: { name: 'room1', description: 'fun', completed: true },
+	room2: { name: 'room2', description: 'scary', completed: false },
+	room3: { name: 'room3', description: 'locked', completed: true },
+};
